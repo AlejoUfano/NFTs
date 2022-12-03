9c5bb08005283ee0290532172788e12b4052d468
Alejo Ufano <ufanoalejo@gmail.com>
2022-12-03 13:10:51 -0300

    Fix(LiveAuction) Component update

--- src/components/UI/LiveAuction/LiveAuction.jsx
+++ src/components/UI/LiveAuction/LiveAuction.jsx
@@ -3,55 +3,30 @@ import { Container, Row, Col } from 'reactstrap'
 import { Link } from 'react-router-dom'
 import img01 from '../../../assets/images/img-01.jpg'
 import ava01 from '../../../assets/images/ava-01.png'
+import './liveAuction.css'
+import { NFT__DATA } from '../../../assets/data/data'
+import NftCard from '../NftCard/NftCard'
 
 const LiveAuction = () => {
   return (
     <section>
         <Container>
             <Row>
-                <Col lg='12' className='mb-4'>
+                <Col lg='12' className='mb-5'>
                     <div className="liveAuctionTop d-flex align-items-center justify-content-between">
                         <h3>Live Auction</h3>
                         <span><Link to='/market'>Explore more</Link></span>
                     </div>
-                </Col>
-                <Col lg='3'>
+                </Col>                
                     
-                    <div className='singleNftCard'>
-                        
-                        <div className="nftImg">
-                            <img src={img01} alt="" className='w-100' />
-                        </div>
-                        
-                        <div className="nftContent">
-                            
-                            <h5 className='nftTitle'>Travel Monkey Club</h5>
-                            <div className="creatorInfoWrapper">
+                    {
+                        NFT__DATA.map(item =>(
+                            <Col lg='3' className='mb-4'>
+                                <NftCard data={item} key={item.id}/>
+                            </Col>
+                        ))
+                    }
 
-                                <div className="creatorImg">
-                                    <img src={ava01} alt="" className='w-100' />
-                                </div>
-                                
-                                <div className="creatorInfo d-flex align-items-center justify-content-between">
-
-                                    <div className='w-50'>
-                                        <h6>Created By</h6>
-                                        <p>Trista Francis</p>
-                                    </div>
-
-                                    <div className='w-50'> 
-                                        <h6>Current Bid</h6>
-                                        <p>3.13 ETH</p>
-                                    </div>
-
-                                </div>
-
-                            </div>
-
-                        </div>
-                    </div>
-
-                </Col>
             </Row>
         </Container>
     </section>
