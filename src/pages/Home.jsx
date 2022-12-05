923cc92d9c5af69dcdd8509ae11d17a7532e4236
Alejo Ufano <ufanoalejo@gmail.com>
2022-12-05 20:01:51 -0300

    Update(Home) SellerSection added

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import HeroSection from '../components/UI/HeroSection'
 import LiveAuction from '../components/UI/LiveAuction/LiveAuction'
+import SellerSection from '../components/UI/SellerSection/SellerSection'
 
 const Home = () => {
   return (
     <>
       <HeroSection />
       <LiveAuction />
+      <SellerSection />
     </>
   )
 }
