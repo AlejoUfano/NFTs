c5072b55c906622b3277a5265fa435bcb42a1137
Alejo Ufano <ufanoalejo@gmail.com>
2022-12-07 18:32:50 -0300

    Update(Home) Section added

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import HeroSection from '../components/UI/HeroSection'
 import LiveAuction from '../components/UI/LiveAuction/LiveAuction'
 import SellerSection from '../components/UI/SellerSection/SellerSection'
+import StepSection from '../components/UI/StepSection/StepSection'
 import TrendingSection from '../components/UI/TrendingSection/TrendingSection'
 
 const Home = () => {
@@ -11,6 +12,7 @@ const Home = () => {
       <LiveAuction />
       <SellerSection />
       <TrendingSection />
+      <StepSection />
     </>
   )
 }
