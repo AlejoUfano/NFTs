47a66611163b6dc65246d7d0c123368872f02e28
Alejo Ufano <ufanoalejo@gmail.com>
2022-12-09 19:17:09 -0300

    Update(NftCard) Updates related to modal

--- src/components/UI/NftCard/NftCard.jsx
+++ src/components/UI/NftCard/NftCard.jsx
@@ -1,9 +1,11 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
+import Modal from '../Modal/Modal'
 import './nftCard.css'
 
 
 const NftCard = ({data}) => {
+    const [showModal, setShowModal] = useState(false)
   return (
     <div className='singleNftCard'>
                         
@@ -38,10 +40,14 @@ const NftCard = ({data}) => {
 
                             <div className='mt-3 d-flex align-items-center justify-content-between'>
 
-                                <button className='bidBtn d-flex align-items-center gap-2'>
+                                <button className='bidBtn d-flex align-items-center gap-1' onClick={()=>setShowModal(true)}>
                                     <i class="ri-shopping-bag-line"></i> 
                                     Place Bid
                                 </button>
+
+                                {
+                                    showModal && <Modal setShowModal={setShowModal} />
+                                }
                                 
                                 <span className='historyLink'><Link to='#'>View History</Link></span>
 
